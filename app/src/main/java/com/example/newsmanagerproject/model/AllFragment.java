292f8acadc0719ea7fc54e35e999b5116d946d4d
CEYL97 <ce.yucra@alumnos.upm.es>
2020-05-21 17:52:47 +0200

    All Fragment

--- app/src/main/java/com/example/newsmanagerproject/model/AllFragment.java
+++ app/src/main/java/com/example/newsmanagerproject/model/AllFragment.java
@@ -18,6 +18,7 @@ import com.example.newsmanagerproject.Login;
 import com.example.newsmanagerproject.R;
 import com.example.newsmanagerproject.database.ArticleDB;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,12 +28,9 @@ import java.util.concurrent.ExecutionException;
 
 public class AllFragment extends Fragment {
 
-    private ListView recyclerView;
-    private NewsAdapter myAdapter;
     private ArrayList<Article>arrayArticle;
     private List<Article> listRes;
-    private FloatingActionButton loginButon;
-    private LoadArticlesTask loadArticlesTask;
+
     public AllFragment(){
 
     }
@@ -41,13 +39,12 @@ public class AllFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View root=inflater.inflate(R.layout.fragment_all,container,false);
-        Intent articles= Objects.requireNonNull(getActivity()).getIntent();
 
-        //Tenemos que realizar llamadas al servicio loadTask
-        loadArticlesTask = new LoadArticlesTask(getContext());
+        //Call loadArticleTask service
+        LoadArticlesTask loadArticlesTask = new LoadArticlesTask(getContext());
         try {
             //add in db
-            listRes=loadArticlesTask.execute().get();
+            listRes= loadArticlesTask.execute().get();
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -55,27 +52,16 @@ public class AllFragment extends Fragment {
         }
 
         // This part will show a list of articles
-        recyclerView = (ListView) root.findViewById(R.id.list_all);
+        ListView recyclerView = root.findViewById(R.id.list_all);
 
-        myAdapter = new NewsAdapter(getContext(), listRes);
+        NewsAdapter myAdapter = new NewsAdapter(Objects.requireNonNull(getContext()), listRes);
         recyclerView.setAdapter(myAdapter);
 
          //This let us set every item clickable LUEGO DESCOMENTARTodo
         recyclerView.setClickable(true);
 
-        recyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-            //When we clicked any  item of the list view. This action will ocurre
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-//                Snackbar.make(view, "Element clicked -> " + position, Snackbar.LENGTH_LONG).show();
-//                Log.i("Click", "click en el elemento " + position + " de mi ListView");
-                //goNewsArticle(view, position);
-            }
-        });
-
-        loginButon = (FloatingActionButton) root.findViewById(R.id.loginButton);
+        FloatingActionButton loginButon = (FloatingActionButton) root.findViewById(R.id.loginButton);
         Log.i("LoginButton","Antes del loginButton");
         loginButon.setOnClickListener(new View.OnClickListener() {
             @Override
