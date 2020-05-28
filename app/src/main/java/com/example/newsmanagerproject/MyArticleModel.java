de1bdc74358c73ee19bc7fedb6f903b8839f62ac
Calollikito97 <ce.yucra@alumnos.upm.es>
2020-05-28 23:28:18 +0200

    Creating view model

--- app/src/main/java/com/example/newsmanagerproject/MyArticleModel.java
+++ app/src/main/java/com/example/newsmanagerproject/MyArticleModel.java
@@ -14,13 +14,25 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MyArticleModel extends ViewModel {
-    private MutableLiveData<List<Article>> articles = new MutableLiveData<>();
+    private MutableLiveData<List<Article>> articles;
+
+    /**
+     * getArticles(): This method initialize list articles data to
+     * visualize in the view components
+     */
 
     public LiveData<List<Article>> getArticles() {
-        loadArticles();
+        if (articles == null) {
+            articles = new MutableLiveData<>();
+            loadArticles();
+        }
         return articles;
     }
 
+    /**
+     * loadArticles(): This method calls getArticles of DB to
+     * get articles in 2 ways. From the article server or from article DB.
+     */
     private void loadArticles() {
         List<Article> res = new ArrayList<Article>();
         List<Article> aux = new ArrayList<Article>(ArticleDB.getArticles());
@@ -29,5 +41,17 @@ public class MyArticleModel extends ViewModel {
             res.add((Article) iterator.next());
         }
         articles.setValue(res);
+    }
+
+    /**
+     * getMoreArticles(): This method load more articles from DB or
+     * article server and add those elements to articles "atribute"
+     */
+    public void getMoreArticles() {
+        List<Article> aux = new ArrayList<Article>(ArticleDB.getArticles());
+        List<Article> res = new ArrayList<>(articles.getValue());
+        res.addAll(aux);
+        articles.postValue(res);
+
     }
 }
